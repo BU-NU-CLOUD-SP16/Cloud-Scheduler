ddd2266b33b66b4c121ae82eed0a313b24280e5c
Kovit Nisar <kovit@ccs.neu.edu>
2016-03-14 23:09:00 -0400

    setting timeout of 10 sec for http endpoint

--- Utilities/src/HTTP.java
+++ Utilities/src/HTTP.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by kovit on 3/10/2016.
@@ -11,7 +13,10 @@ public final class HTTP {
     private HTTP() {}
 
     public static String executeRequest(String url) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+        URLConnection conn = new URL(url).openConnection();
+        conn.setConnectTimeout(10000);
+        conn.setReadTimeout(10000);
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         String strTemp = "";
         StringBuilder sb = new StringBuilder();
         while (null != (strTemp = br.readLine())) {
