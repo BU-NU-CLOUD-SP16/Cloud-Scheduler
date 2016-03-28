58e946f379dfe5a797ce76243f685a613fb240be
praveenkmurthy <keshavamurthy.p@husky.neu.edu>
2016-03-27 20:04:16 -0400

    Working Overlord Framework

--- Overlord/src/HttpInterface.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Sanath on 3/26/2016.
- */
-public interface HttpInterface {
-}
---
+++ Overlord/src/JSONRegisterCEAgent.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Praveen on 3/27/2016.
+ */
+public class JSONRegisterCEAgent {
+
+    private Integer ceAgentID;
+
+    public Integer getCeAgentID() {
+        return ceAgentID;
+    }
+
+    public void setCeAgentID(Integer ceAgentID) {
+        this.ceAgentID = ceAgentID;
+    }
+}
