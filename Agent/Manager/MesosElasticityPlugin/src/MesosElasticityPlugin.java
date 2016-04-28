ae66494ce12c5267b8de30a22b6a7bd0ca43accf
Sourabh Bollapragada <chemistry.sourabh@yahoo.co.in>
2016-04-27 23:07:30 -0400

    Single SSH

--- Agent/Manager/MesosElasticityPlugin/src/MesosElasticityPlugin.java
+++ Agent/Manager/MesosElasticityPlugin/src/MesosElasticityPlugin.java
@@ -451,6 +451,9 @@ public class MesosElasticityPlugin implements ElasticityPlugin {
         String s8 = "sudo perl -i -p0e 's/\\/\\*\\nudp_send_channel {.*?#mcast_join = 239.2.11.71.*?host = localhost.*?port = 8649 .*?ttl = 1 .*?}.*?\\*\\/\\n/udp_send_channel {\\nhost = "+hdfsIp+"\\nport=8649\\nttl=1\\n}\\n/s' /etc/ganglia/gmond.conf";
         String s9 = "sudo service ganglia-monitor start";
 
+
+        String s = s4 + " && "+s1+" && "+s2+ " && "+s5+" && "+s6+" && "+s3;
+
         SshProxy proxy = new SshProxy(sshHost,sshPort,privateKey);
 
         int timeout = 5000;
@@ -466,38 +469,47 @@ public class MesosElasticityPlugin implements ElasticityPlugin {
             {
                 proxy.closeSessions();
                 logger.log(Level.SEVERE,e.getMessage(),GlobalLogger.MANAGER_LOG_ID);
-                if (e.getMessage().equalsIgnoreCase("timeout: socket is not established"))
+                if (e.getMessage().contains("No Route") || e.getMessage().contains("Connection Refused"))
                 {
-                    timeout = 40000;
-                    logger.log(Level.INFO,"New Timeout = "+timeout,GlobalLogger.MANAGER_LOG_ID);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                    continue;
                 }
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e1) {
-                    e1.printStackTrace();
+
+                else
+                {
+                    break;
                 }
+//                if (e.getMessage().equalsIgnoreCase("timeout: socket is not established"))
+//                {
+//                    timeout = 40000;
+//                    logger.log(Level.INFO,"New Timeout = "+timeout,GlobalLogger.MANAGER_LOG_ID);
+//                }
             }
         }
 
         int exit;
-        exit = tryExecutingForever(proxy,openStackNode.getIp(),s4);
-        logger.log(Level.INFO, "Executed " + s4 + " with status " + exit, GlobalLogger.MANAGER_LOG_ID);
-        exit = tryExecutingForever(proxy,openStackNode.getIp(),s1);
-        logger.log(Level.INFO,"Executed "+s1+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
-        exit = tryExecutingForever(proxy,openStackNode.getIp(),s2);
-        logger.log(Level.INFO,"Executed "+s2+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
-        exit = tryExecutingForever(proxy,openStackNode.getIp(),s5);
-        logger.log(Level.INFO,"Executed "+s5+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
-        exit = tryExecutingForever(proxy,openStackNode.getIp(),s6);
-        logger.log(Level.INFO,"Executed "+s6+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
-//        exit = tryExecutingForever(proxy,openStackNode.getIp(),s7);
-//        logger.log(Level.INFO,"Executed "+s7+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
-//        exit = tryExecutingForever(proxy,openStackNode.getIp(),s8);
-//        logger.log(Level.INFO,"Executed "+s8+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
-//        exit = tryExecutingForever(proxy,openStackNode.getIp(),s9);
-//        logger.log(Level.INFO,"Executed "+s9+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
-        exit = tryExecutingForever(proxy,openStackNode.getIp(),s3);
-        logger.log(Level.INFO,"Executed "+s3+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
+        exit = tryExecutingForever(proxy,openStackNode.getIp(),s);
+        logger.log(Level.INFO, "Executed " + s + " with status " + exit, GlobalLogger.MANAGER_LOG_ID);
+//        exit = tryExecutingForever(proxy,openStackNode.getIp(),s1);
+//        logger.log(Level.INFO,"Executed "+s1+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
+//        exit = tryExecutingForever(proxy,openStackNode.getIp(),s2);
+//        logger.log(Level.INFO,"Executed "+s2+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
+//        exit = tryExecutingForever(proxy,openStackNode.getIp(),s5);
+//        logger.log(Level.INFO,"Executed "+s5+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
+//        exit = tryExecutingForever(proxy,openStackNode.getIp(),s6);
+//        logger.log(Level.INFO,"Executed "+s6+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
+////        exit = tryExecutingForever(proxy,openStackNode.getIp(),s7);
+////        logger.log(Level.INFO,"Executed "+s7+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
+////        exit = tryExecutingForever(proxy,openStackNode.getIp(),s8);
+////        logger.log(Level.INFO,"Executed "+s8+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
+////        exit = tryExecutingForever(proxy,openStackNode.getIp(),s9);
+////        logger.log(Level.INFO,"Executed "+s9+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
+//        exit = tryExecutingForever(proxy,openStackNode.getIp(),s3);
+//        logger.log(Level.INFO,"Executed "+s3+" with status "+exit,GlobalLogger.MANAGER_LOG_ID);
 
 
 //            exit = proxy.executeCommand(openStackNode.getIp(),s0);
@@ -511,18 +523,18 @@ public class MesosElasticityPlugin implements ElasticityPlugin {
 
     private int tryExecutingForever(SshProxy proxy,String ip, String command) {
         int exit = 0;
-        int timeout = 5000;
+        int timeout = 60000;
         while (true) {
             try {
                 exit = proxy.executeCommand(ip, command,timeout);
-                Thread.sleep(1000);
+//                Thread.sleep(1000);
                 break;
             } catch (Exception ex) {
                 proxy.closeSessions();
                 logger.log(Level.SEVERE,ex.getMessage(),GlobalLogger.MANAGER_LOG_ID);
                 if (ex.getMessage().equalsIgnoreCase("timeout: socket is not established"))
                 {
-                    timeout = 40000;
+                    timeout += 5000;
                     logger.log(Level.INFO,"New Timeout = "+timeout,GlobalLogger.MANAGER_LOG_ID);
                 }
                 try {
